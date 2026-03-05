ae648ca027a403c8deb23f22e81151e659ff78d9
Siraphop Sukchu <contact@siraphop.me>
2026-03-05 19:10:52 +0700

    Add area 3D action title for car

--- Model/car.gd
+++ Model/car.gd
@@ -3,7 +3,12 @@ extends VehicleBody3D
 @export var MAX_STEER = 0.9
 @export var ENGINE_POWER = 200
 
+@onready var title_action: Label3D = $TitleAction
+
 var is_driven: bool = false 
+
+func _ready() -> void:
+	title_action.hide()
 
 func _physics_process(delta):
 	if is_driven:
@@ -12,3 +17,11 @@ func _physics_process(delta):
 	else:
 		steering = move_toward(steering, 0, delta * 10)
 		engine_force = 0
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player") :
+		title_action.show()
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player") :
+		title_action.hide()
