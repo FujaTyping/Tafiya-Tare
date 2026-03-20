62e648a0b745e4beb6f056b2f601da1b4652fa0e
Siraphop Sukchu <contact@siraphop.me>
2026-03-20 18:26:28 +0700

    Add more food

--- Item/Ingredient/cooking_put.gd
+++ Item/Ingredient/cooking_put.gd
@@ -11,10 +11,9 @@ func level3cooking() :
 	isCooking = true
 	await Varibles.wait(10)
 	if "floor" in isInpot and "egg" in isInpot and isInpot.size() == 2 :
-		var rewardItem = listArrayMenu[1].instantiate()
-		get_tree().current_scene.add_child(rewardItem)
-		rewardItem.completeQuestReward = 20
-		rewardItem.global_transform = item_spawn.global_transform
+		spawnFood(listArrayMenu[1])
+	elif "potato" in isInpot and "cucumber" in isInpot and "bellPeper" in isInpot and isInpot.size() == 3 :
+		spawnFood(listArrayMenu[2])
 	else :
 		var rewardItem = listArrayMenu[0].instantiate()
 		get_tree().current_scene.add_child(rewardItem)
@@ -22,6 +21,11 @@ func level3cooking() :
 	finish.play()
 	isCooking = false
 	isInpot = []
+
+func spawnFood(ArrayIndex: PackedScene) :
+	var rewardItem = ArrayIndex.instantiate()
+	get_tree().current_scene.add_child(rewardItem)
+	rewardItem.global_transform = item_spawn.global_transform
 
 func interact() :
 	if isCooking :
