48668140809d24e1a3c70067afae90d583fbd226
Siraphop Sukchu <contact@siraphop.me>
2026-06-12 20:30:40 +0700

    Add tafiya tare content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
@@ -7,9 +7,16 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   adapter: cloudflare(),
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  fonts: [{
+    provider: fontProviders.fontsource(),
+    name: "Roboto Condensed",
+    cssVariable: "--font-roboto-condensed",
+  }]
 });
